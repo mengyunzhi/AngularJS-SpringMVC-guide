5c206c9e2d104ed531408402116629a4e022fd6c
teacherPan <3792535@qq.com>
2017-11-24 18:45:06 +0800

    step4

--- webApp/app/scripts/controllers/main.js
+++ webApp/app/scripts/controllers/main.js
@@ -9,29 +9,31 @@
  * Controller of the webAppApp
  */
 angular.module('webAppApp')
-  .controller('MainCtrl', function ($scope) {
-  	var lists = [
-  		{
-  			id: 1,
-  			username: 'zhangsan',
-  			name: '张三',
-  			email: 'zhangsan@yunzhiclub.com',
-  			sex: '男'
-  		},
-  		{
-  			id: 2,
-  			username: 'lisi',
-  			name: '李四',
-  			email: 'lisi@yunzhiclub.com',
-  			sex: '女'
-  		}
-  	];
-  	console.log(lists);
-  	$scope.lists = lists;
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    console.log('hello world1!');
+  .controller('MainCtrl', function ($scope, $http) {
+    // 定义url请求地址
+    var url = '/data/main.json';
+    
+    // 定义一个方法，名字为success , 参数为response
+    var success = function(response) {
+      $scope.lists = response.data;
+    
+      console.log('success');
+    };
+
+    /**
+     * 定义一个error方法
+     * @return {[type]} [description]
+     */
+    var error = function() {
+      console.log('error');
+    };
+
+    // 发起请求，并获取一个promise对象
+    var promise = $http.get(url);
+
+    // 调用这个对象的then方法，当上面的请求成功时，调用第一个参数（success),并且把请求的信息给sucess方法的第一个参数
+    // 当请求失败时，调用第二个方法 error
+    promise.then(success, error);
+
+
   });
