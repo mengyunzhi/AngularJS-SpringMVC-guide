373b8ef1823eb966fc0ebb85a80095ebb2b4ef52
teacherPan <3792535@qq.com>
2017-11-25 18:01:18 +0800

    step11

--- webApp/app/scripts/app.js
+++ webApp/app/scripts/app.js
@@ -15,26 +15,24 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-    // 注册一个路由地址为： /
-      .when('/', {
-        templateUrl: 'views/main.html', // v层文件位置为：views/main.html
-        controller: 'MainCtrl',  // C层为 MainCtrl
-        controllerAs: 'main'
-      })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state({
+      name: 'main', // 名称
+      url: '/main', 
+      controller: 'MainCtrl', // 控制器名称
+      templateUrl: 'views/main.html' // V层名称
+    })
+    .state({
+      name: 'about', // 名称
+      url: '/about', 
+      controller: 'AboutCtrl', // 控制器名称
+      templateUrl: 'views/about.html' // V层名称
+    });
 
-      // 注册了一个路由,地址为：/about
-      .when('/about', {
-        templateUrl: 'views/about.html',  // 
-        controller: 'AboutCtrl',  // 
-        controllerAs: 'about' // 
-      })
-
-      // 当没有找到注册的路由时，那么跳转至默认路由 /
-      .otherwise({
-        redirectTo: '/'
-      });
+    $urlRouterProvider.otherwise('/main');
+   
   });
