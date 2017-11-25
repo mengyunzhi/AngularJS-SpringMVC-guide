f8ac4774754a219d515f18815935257f2da5bf09
teacherPan <3792535@qq.com>
2017-11-25 11:30:09 +0800

    step5

---
+++ api/SpringMvcStudy/src/main/java/com/mengyunzhi/SpringMvcStudy/SpringMvcStudyApplication.java
@@ -0,0 +1,12 @@
+package com.mengyunzhi.SpringMvcStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcStudyApplication.class, args);
+	}
+}
