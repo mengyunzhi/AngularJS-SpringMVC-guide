5527724006ae0af6a44ece817546ca74ae2bca45
teacherPan <3792535@qq.com>
2017-12-12 18:32:20 +0800

    step2.4.1

--- webApp/app/scripts/controllers/main.js
+++ webApp/app/scripts/controllers/main.js
@@ -9,10 +9,11 @@
  * Controller of the webAppApp
  */
 angular.module('webAppApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function($scope, $http) {
+    var self = this;
     // 定义url请求地址
     var url = 'http://127.0.0.1:8080/Teacher';
-    
+
     // 定义一个方法，名字为success , 参数为response
     var success = function(response) {
       $scope.lists = response.data;
@@ -33,6 +34,18 @@ angular.module('webAppApp')
     // 调用这个对象的then方法，当上面的请求成功时，调用第一个参数（success),并且把请求的信息给sucess方法的第一个参数
     // 当请求失败时，调用第二个方法 error
     promise.then(success, error);
+
+    self.delete = function(teacher) {
+       var url = 'http://127.0.0.1:8080/Teacher/' + teacher.id;
+       $http.delete(url)
+       .then(function success(){
+          console.log('删除数据成功');
+       }, function error(){
+          console.log('error');
+       });
+    };
+
+    $scope.delete = self.delete;
 
 
   });
