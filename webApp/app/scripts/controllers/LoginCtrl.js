ecc1602ed374b13ceb2813a0a8f869ff2fabdb2f
teacherPan <3792535@qq.com>
2018-10-18 16:18:58 +0800

    step6.7

--- webApp/app/scripts/controllers/LoginCtrl.js
+++ webApp/app/scripts/controllers/LoginCtrl.js
@@ -8,7 +8,7 @@
  * 登录控制器
  */
 angular.module('webAppApp')
-    .controller('LoginCtrl', function($scope, teacher) {
+    .controller('LoginCtrl', function($scope, teacher, $location) {
         var self = this;
 
         self.init = function() {
@@ -16,13 +16,18 @@ angular.module('webAppApp')
         		username: '',
         		password: ''
         	};
+
+        	// 显示出错信息
+        	$scope.showErrorBox = false;
         };
 
         self.submit = function() {
         	teacher.login($scope.user)
         	.then(function success(response) {
-        		console.log('success', response);
+        		$scope.showErrorBox = false;
+        		$location.url('/main');
         	}, function error(response) {
+        		$scope.showErrorBox = true;
         		console.log('error', response);
         	});
         };
